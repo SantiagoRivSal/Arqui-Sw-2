d14ccbe03f09ea287654fda1781b733b31e3af84
SantiagoRivSal <santiagosalomon2001@gmail.com>
2022-11-12 16:27:20 -0300

    Se pusieron los parametros en los botones para las query

--- frontend/src/Componentes/Header/index.js
+++ frontend/src/Componentes/Header/index.js
@@ -1,40 +1,8 @@
 import React, { useContext, useEffect, useState} from "react";
 import Libertador from "../../images/logo.jpeg";
-//import SearchCountry from "./Topics";
-//import SearchCity from "./Topics";
-//import SearchService from "./Topics";
 import './header.css';
 
 export const Header = () => {
-  const [SearchCountries,setSearchContry] = useState([]);
-  const [SearchCities,setSearchCity] = useState([]);
-  const [SearchServices,setSearchService] = useState([]);
-  const fetchApi1 = async()=>{
-    const response = await fetch('http://localhost:8090/properties/country')
-    .then((response) => response.json());
-    setSearchContry(response);
-    };
-    useEffect(()=>{
-    fetchApi1();
-    },[])
-
-    const fetchApi2 = async()=>{
-      const response = await fetch('http://localhost:8090/properties/city')
-      .then((response) => response.json());
-      setSearchCity(response);
-      };
-      useEffect(()=>{
-      fetchApi2();
-      },[])
-
-      const fetchApi3 = async()=>{
-        const response = await fetch('http://localhost:8090/properties/service')
-        .then((response) => response.json());
-        setSearchService(response);
-        };
-        useEffect(()=>{
-        fetchApi3();
-        },[])
   return (
     <header class="header">
       <div class="logo-div">
@@ -42,49 +10,6 @@ export const Header = () => {
       </div>
       <div class="buscar">
         BUSCA TU HOGAR IDEAL
-      </div>
-      <div class="search-padre">
-        <div class="search-hijo">
-          <div class="select">
-            <select list="pais-PT" name="pais-PT" id="pais-PT">
-            <option value="" selected disabled>Pais</option>
-            {
-                SearchCountries.map(country =>(
-                  <option value="">{country}</option> 
-                ))
-            }
-            </select>
-          </div>
-          <div class="lista">
-            <div class="select">
-              <select list="ciudad-PT" name="ciudad-PT" id="ciudad-PT">
-              <option value="" selected disabled>Ciudad</option>
-            {
-                SearchCities.map(city =>(
-                  <option value="">{city}</option> 
-                ))
-            }
-              </select>
-            </div>
-          </div>
-          <div class="lista">
-            <div class="select">
-              <select list="ciudad-PT" name="ciudad-PT" id="ciudad-PT">
-              <option value="" selected disabled>Servicio</option>
-            {
-                SearchServices.map(service =>(
-                  <option value="">{service}</option> 
-                ))
-            }
-              </select>
-            </div>
-          </div>
-          <div class="div-search">
-            <button class="btn-search" type="submit">
-              Buscar
-            </button>
-          </div>
-        </div>
       </div>
     </header>
   )
--- frontend/src/Componentes/Main/index.js
+++ frontend/src/Componentes/Main/index.js
@@ -1,31 +1,113 @@
 import React, { useContext, useEffect, useState} from "react";
-import { PropertyItems } from "./PropertyItems";
-
+import { PropertyItems } from "../Main/PropertyItems";
+import swal from "sweetalert2";
 import './main.css';
 
 
 
 export const Main = () => {
   const [properties,setProperties] = useState([]);
-
-  const fetchApi = async()=>{
-    const response = await fetch('http://localhost:8090/properties/all')
+  const [SearchCountries,setSearchContry] = useState([]);
+  const [SearchCities,setSearchCity] = useState([]);
+  const [SearchServices,setSearchService] = useState([]);
+  const [valueCity, setValueCity] = useState("");
+  const [valueCountry, setValueCountry] = useState("");
+  const [valueService, setValueService] = useState("");
+  const fetchApi1 = async()=>{
+    const response = await fetch('http://localhost:8090/properties/country')
     .then((response) => response.json());
-    setProperties(response);
+    setSearchContry(response);
     };
     useEffect(()=>{
-    fetchApi();
+    fetchApi1();
     },[])
+
+    const fetchApi2 = async()=>{
+      const response = await fetch('http://localhost:8090/properties/city')
+      .then((response) => response.json());
+      setSearchCity(response);
+      };
+      useEffect(()=>{
+      fetchApi2();
+      },[])
+
+      const fetchApi3 = async()=>{
+        const response = await fetch('http://localhost:8090/properties/service')
+        .then((response) => response.json());
+        setSearchService(response);
+        };
+        useEffect(()=>{
+        fetchApi3();
+        },[])
+
+      const fetchApiProperty = async()=>{
+          const response = await fetch("http://localhost:8983/solr/Properties/select?defType=lucene&fq=city%3A%22"+valueCity+"%22&fq=country%3A%22"+valueCountry+"%22&fq=service%3A%22"+valueService+"%22&indent=true&q.op=OR&q=*%3A*")
+          .then((response) => response.json());
+          setProperties(response)
+          console.log(response);
+          };
+  
+      const handleChange=e=>{
+      setValueCity(e.target.value);
+       setValueCountry(e.target.value);
+       setValueService(e.target.value);
+        };
+  
+      const handleSubmit= (event)=>{
+          event.preventDefault();
+          fetchApiProperty();
+      };
   return (
     <main>
-      <div class="baner">
-        <p>
-          BIENVENIDOS A LIBERTADOR INMOBILIARIOS
-        </p>
+        <div class="search-padre">
+        <div class="search-hijo">
+          <div class="select">
+            <select list="pais-PT" name="pais-PT" id="pais-PT" valueCountry={ valueCountry } onChange={ (event) => setValueCountry(event.target.value)}>
+            <option value="" selected disabled>Pais</option>
+            {
+                SearchCountries.map(country =>(
+                  <option value={country} onChange={handleChange}>{country}
+         </option> 
+                ))
+            }
+            </select>
+          </div>
+          <div class="lista">
+            <div class="select">
+              <select list="ciudad-PT" name="ciudad-PT" id="ciudad-PT" valueCity={ valueCity } onChange={ (event) => setValueCity(event.target.value)}>
+              <option value="" selected disabled>Ciudad</option>
+            {
+                SearchCities.map(city =>(
+                  <option value={city} onChange={handleChange}>{city}
+                  </option> 
+                ))
+            }
+              </select>
+            </div>
+          </div>
+          <div class="lista">
+            <div class="select">
+              <select list="ciudad-PT" name="ciudad-PT" id="ciudad-PT" valueService={ valueService } onChange={ (event) => setValueService(event.target.value)}>
+              <option value="" selected disabled>Servicio</option>
+            {
+                SearchServices.map(service =>(
+                  <option value={service} onChange={handleChange}>{service}
+                  </option> 
+                ))
+            }
+              </select>
+            </div>
+          </div>
+          <div class="div-search">
+            <button class="btn-search" type="button" onClick = {handleSubmit}>
+              Buscar
+            </button>
+          </div>
+        </div>
       </div>
       <div className="Property">
       {
-                properties.map(property =>(
+                properties.map((property) =>(
                   <PropertyItems key={property.id}
                   id={property.id}
                   tittle ={property.tittle}
@@ -43,7 +125,6 @@ export const Main = () => {
                   /> 
                 ))
             }
-
         </div>
     </main>
   )
