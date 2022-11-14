016809bf2bf63d3938b6963a4dd20a9d49131a72
Matías Lessio <2003159@ucc.edu.ar>
2022-11-14 09:59:31 -0300

    get by id y descarga

--- microservicio/router/router.go
+++ microservicio/router/router.go
@@ -9,15 +9,11 @@ import (
 
 func MapUrls(router *gin.Engine) {
 	// Products Mapping
-	//router.GET("/properties/:id", propertyController.Get)
-	router.GET("/properties/:param", propertyController.GetByParam)
+	router.GET("/properties/:parameters/id", propertyController.Get)
+	router.GET("/properties/:parameters", propertyController.GetByParam)
 	router.GET("/properties/all", propertyController.GetAll)
 	router.POST("/properties/load", propertyController.Insert)
 	router.POST("/properties/import", propertyController.InsertMany)
 
-	//router.GET("/propertyRandom/:cantidad", propertyController.GetRandom)
-	//router.GET("/properties/country", propertyController.GetCounty)
-
-	//router.GET("/properties/service", propertyController.GetService)
 	fmt.Println("Finishing mappings configurations")
 }
